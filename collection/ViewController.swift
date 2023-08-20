429217bff146a8129d2e264a07ab6a92f8e8cb4e
AzharaKalyk <kab_180495@mail.ru>
2023-08-21 00:00:15 +0600

    Initial Commit

---
+++ collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collection
+//
+//  Created by Azhar Kalyk on 21.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
